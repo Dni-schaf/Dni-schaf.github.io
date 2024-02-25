a1fc811935e48983c63f88b18e9bf8cd5e6964c0
Dni-schaf <d.scharffenberg@web.de>
2024-02-25 09:54:25 +0100

    total new index site

---
+++ resources/js/diashow.js
@@ -0,0 +1,20 @@
+const images = document.querySelectorAll('img');
+        let currentImage = 0;
+
+        function changeImage() {
+            
+            setTimeout(() => {
+                images[currentImage].classList.remove('active');
+                images[currentImage].classList.add('before');
+                beforimage = currentImage;
+                currentImage = (currentImage + 1) % images.length;
+                images[currentImage].classList.remove('next');
+                images[currentImage].classList.add('active');
+                setTimeout(() => {
+                    images[beforimage].classList.remove('before');
+                    images[beforimage].classList.add('next');
+                }, 1000);
+
+            }, 1000);
+        }
+        setInterval(changeImage, 3000);
