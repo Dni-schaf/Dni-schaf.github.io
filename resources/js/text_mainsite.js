d0fbeb69836ac2b4e91d152ff11ce398ad2d1671
Dni-schaf <d.scharffenberg@web.de>
2024-03-26 18:15:28 +0100

    update nowegan text

--- resources/js/text_mainsite.js
+++ resources/js/text_mainsite.js
@@ -4,7 +4,7 @@ const txt =
         "Kapitel": "headline",
         "de": "Ein Palast aus Eis am Ende der Welt",
         "en": "A Palace of Ice at the World's End",
-        "no": "Et Palass av Is ved Verdens Ende"
+        "no": "Et palass av is ved verdens ende"
     },
     {
         "Kapitel": "ProjektH2",
@@ -16,7 +16,7 @@ const txt =
         "Kapitel": "ProjektP",
         "de": "Willkommen bei unserem interaktiven Web-Comic über den epischen Wettlauf zum Südpol! Inspiriert von den fesselnden Tagebüchern von Amundsen und Scott haben wir uns zusammengetan, um eine Geschichte zu erzählen, die alles hat, was eine gute Geschichte braucht: Abenteuer, Spannung und eine faszinierende Konfrontation zweier unerschrockener Männer, die das gleiche Ziel verfolgen, aber unterschiedlicher nicht sein könnten.",
         "en": "Welcome to our interactive web comic about the epic race to the South Pole! Inspired by the captivating diaries of Amundsen and Scott, we've come together to tell a story that has everything a good tale needs: adventure, suspense, and a captivating clash between two fearless men pursuing the same goal but couldn't be more different.",
-        "no": "Velkommen til vår interaktive web-tegneserie om det episke kappløpet til Sydpolen! Inspirert av de fengslende dagbøkene til Amundsen og Scott, har vi slått oss sammen for å fortelle en historie som har alt hva en god fortelling trenger: eventyr, spenning og en fengende konfrontasjon mellom to fryktløse menn som forfølger det samme målet, men som ikke kunne vært mer ulike."
+        "no": "Velkommen til vår forhåndsvisning av en ny interaktiv opplevelse om kappløpet til Sydpolen! Basert på dagbøker og historiske kilder fra Amundsen og Scott, jobber vi med å presentere historien, spenning og det dramatisk kappløpet på en ny og interaktiv måte."
     },
     {
         "Kapitel": "ConstructionH2",
@@ -28,37 +28,37 @@ const txt =
         "Kapitel": "ConstructionP",
         "de": "Unser Comic und diese Seite sind noch im Werden. Wir aktualisieren sie ständig mit neuen Entwicklungen. Schauen Sie öfter mal vorbei, um zu sehen, was sich Neues auf unserer Reise getan hat!",
         "en": "Our comic and this website are still a work in progress. We're constantly updating them with new developments. Be sure to check back often to see what's new on our journey!",
-        "no": "Vår tegneserie og denne nettsiden er fortsatt under arbeid. Vi oppdaterer dem kontinuerlig med nye utviklinger. Sørg for å sjekke tilbake ofte for å se hva som er nytt på vår reise!"
+        "no": "Prosjektet er under arbeid og vi oppdaterer denne nettsiden kontinuerlig."
     },
     {
         "Kapitel": "ConstructionDate",
-        "de": "Zuletzt aktualisiert am: 10.03.2024",
-        "en": "Last updated on: 10th March 2024.",
-        "no": "Sist oppdatert: 10. mars 2024."
+        "de": "Zuletzt aktualisiert am: 26.03.2024",
+        "en": "Last updated on: 26th March 2024.",
+        "no": "Sist oppdatert: 26. mars 2024."
     },
     {
         "Kapitel": "DankeH2",
         "de": "Wir danken all den Menschen, die uns unterstützen",
         "en": "We Thank All Those Who Support Us",
-        "no": "Vi takker alle som støtter oss"
+        "no": "Vi takker for all støtte"
     },
     {
         "Kapitel": "DankeP",
         "de": "Unser Projekt begann als eine Idee, geboren aus der Faszination für die Geschichte und den Wunsch, sie auf eine einzigartige und interaktive Weise zum Leben zu erwecken. Was als kleines Nebenprojekt begann, entwickelte sich im Laufe der Jahre zu einem umfangreichen und fesselnden Web-Comic. Mit der Zeit haben viele talentierte Menschen dazu beigetragen, unsere Vision Wirklichkeit werden zu lassen.",
         "en": "Our project started as an idea born out of fascination for history and the desire to bring it to life in a unique and interactive way. What began as a small side project has evolved over the years into a comprehensive and captivating web comic. Along the way, many talented individuals have contributed to making our vision a reality.",
-        "no": "Vårt prosjekt startet som en idé født av fascinasjonen for historie og ønsket om å bringe den til live på en unik og interaktiv måte. Det som begynte som et lite sideprosjekt har utviklet seg over årene til en omfattende og fengslende nettserie. Underveis har mange talentfulle individer bidratt til å gjøre vår visjon til virkelighet."
+        "no": "Vårt prosjekt startet som en idé om en digital fortelling, men gjennom et tett samarbeid med flere fageksperter har prosjektet utviklet seg til en interaktiv opplevelse."
     },
     {
         "Kapitel": "InteraktivH2",
         "de": "Exclusive Vorschau auf die ersten interaktiven Kapitel",
         "en": "Exclusive Preview of the First Interactive Chapters",
-        "no": "Exklusiv forhåndsvisning av de første interaktive kapitlene"
+        "no": "Forhåndsvisning av de første interaktive kapitlene"
     },
     {
         "Kapitel": "InteraktivP",
         "de": "Was unser Projekt besonders macht, ist die Integration interaktiver Infografiken, die zeigen, wo die Expeditionen an welchen Tagen waren, wie die Teams zusammengesetzt waren und wie ein Sextant funktioniert. Darüber hinaus bieten wir 3D-Modelle der Expeditionsschiffe, die es den Lesern ermöglichen, noch tiefer in die Welt des Südpol-Wettlaufs einzutauchen.",
         "en": "What sets our project apart is the integration of interactive infographics that show where the expeditions were on each day, how the teams were composed, and how a sextant works. Additionally, we provide 3D models of the expedition ships, allowing readers to delve even deeper into the world of the South Pole race.",
-        "no": "Det som gjør prosjektet vårt spesielt, er integrasjonen av interaktive infografikk som viser hvor ekspedisjonene var på hvilke dager, hvordan lagene var sammensatt, og hvordan en sekstant fungerer. I tillegg tilbyr vi 3D-modeller av ekspedisjonsskipene som lar leserne dykke enda dypere inn i verdenen til kappløpet mot Sørpolen."
+        "no": "Prosjektet baserer seg på interaktiv infografikk, som blant annet innebærer at du kan følge ekspedisjonene dag for dag, utforske menneskene, utstyret, gå om bord i skipene og være med på sledeferden."
     },
     {
         "Kapitel": "Link1",
@@ -82,18 +82,18 @@ const txt =
         "Kapitel": "ComicH2",
         "de": "Exclusive Vorschau auf das Comic",
         "en": "Exclusive Preview of the Comic",
-        "no": "Eksklusiv forhåndsvisning av tegneserien"
+        "no": "Forhåndsvisning av opplevelsen"
     },
     {
         "Kapitel": "ComicP",
         "de": "Obwohl wir unser erstes Projekt dieser Art sind und viel gelernt haben, seit wir 2020 begonnen haben, haben wir keine bestimmten Erwartungen an das Projekt. Wir tun dies aus purer Leidenschaft und hoffen, dass unsere Leser genauso viel Freude daran haben, unsere Geschichte zu erleben, wie wir es hatten, sie zu erzählen.",
         "en": "Although we are embarking on our first project of this kind and have learned a lot since we began in 2020, we have no specific expectations for the project. We do this out of pure passion and hope that our readers will derive as much joy from experiencing our story as we had in telling it.",
-        "no": "Selv om dette er vårt første prosjekt av denne typen, og vi har lært mye siden vi begynte i 2020, har vi ingen spesifikke forventninger til prosjektet. Vi gjør dette av ren lidenskap og håper at leserne våre vil få like mye glede av å oppleve vår historie som vi hadde det med å fortelle den."
+        "no": "Vi gjør dette av ren lidenskap og håper du vil få like mye glede av å oppleve historien, som det vi har fått av å lage den."
     },
     {
         "Kapitel": "ComicP2",
         "de": "Vielen Dank, dass Sie Teil unserer Reise sind, und wir hoffen, dass Sie unser Werk genießen!",
         "en": "Thank you for being part of our journey, and we hope you enjoy our work!",
-        "no": "Takk for at du er en del av reisen vår, og vi håper du vil sette pris på arbeidet vårt!"
+        "no": " "
     }
 ]
