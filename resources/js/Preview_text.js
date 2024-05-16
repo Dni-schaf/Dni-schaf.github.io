80e2a22d80d4ef6c955abb5adaa56789868bbf92
Dni-schaf <d.scharffenberg@web.de>
2024-05-16 20:20:18 +0200

    norwegan texts

--- resources/js/Preview_text.js
+++ resources/js/Preview_text.js
@@ -3,81 +3,98 @@ const previewTxt =
     {
         "Kapitel": "1_TN_01_01_1",
         "de": "Kapitel 1",
-        "en": "Chapter 1"
+        "en": "Chapter 1",
+        "no": "Kapittel 1"
     },
     {
         "Kapitel": "1_TN_01_01_2",
         "de": "Aus heiterem Himmel",
-        "en": "Out of the blue"
+        "en": "Out of the blue",
+        "no": "Ut av det blå"
     },
     {
         "Kapitel": "1_TN_01_01_3",
         "de": "Melbourne, Australien, 11. Oktober 1910",
-        "en": "Melbourne, Australia, 11 October 1910"
+        "en": "Melbourne, Australia, 11 October 1910",
+        "no": "Melbourne, Australia, 11. oktober 1910"
     },
     {
         "Kapitel": "1_TN_01_02",
         "de": "Bitte, Kapitän, es ist äußerst dringend, sonst wären wir nicht hier!",
-        "en": "Please, Captain, it's extremely urgent, otherwise we wouldn't be here!"
+        "en": "Please, Captain, it's extremely urgent, otherwise we wouldn't be here!",
+        "no": "Vær så snill, kaptein, det er svært viktig, ellers hadde vi ikke vært her!"
     },
     {
         "Kapitel": "1_TN_01_03",
         "de": "Die Terra Nova ankert nur wenige Meilen von hier. Wir müssen unbedingt heute noch dorthin. Sir.",
-        "en": "The Terra Nova is anchored just a few miles from here. We need to get there today. Sir."
+        "en": "The Terra Nova is anchored just a few miles from here. We need to get there today. Sir.",
+        "no": "Terra Nova ligger for anker bare noen få kilometer herfra. Vi må komme dit i dag. Herr."
     },
     {
         "Kapitel": "1_TN_01_04",
         "de": "Die See ist rau, sage ich Ihnen und bald ist es stockfinster.",
-        "en": "The sea is rough, I tell you, and soon it will be pitch dark."
+        "en": "The sea is rough, I tell you, and soon it will be pitch dark.",
+        "no": "Havet er urolig, hører du, og snart vil det være helt mørkt"
     },
     {
         "Kapitel": "1_TN_01_05",
         "de": "Bitte lassen Sie es uns doch wenigstens versuchen!",
-        "en": "Please let us at least give it a try!"
+        "en": "Please let us at least give it a try!",
+        "no": "Vær så snill, la oss i det minste gi det et forsøk"
     },
     {
         "Kapitel": "1_TN_01_06",
         "de": "Unter diesen Bedingungen ist das nicht ganz ungefährlich",
-        "en": "Under these conditions, it is not entirely safe"
+        "en": "Under these conditions, it is not entirely safe",
+        "no": "Under disse forholdene er det ikke helt trygt"
+
     },
     {
         "Kapitel": "1_TN_01_07",
         "de": "Wir müssen unbedingt zu meinem Mann, Kapitän.",
-        "en": "We absolutely have to see my husband, Captain."
+        "en": "We absolutely have to see my husband, Captain.",
+        "no": "Vi må absolutt treffe min mann, kaptein"
     },
     {
         "Kapitel": "1_TN_01_08",
         "de": "Naaa gut ...",
-        "en": "Welll okey ..."
+        "en": "Welll okey ...",
+        "no": "Velll, greit ..."
     },
     {
         "Kapitel": "1_TN_01_09",
         "de": "... aber nur auf Ihre Verantwortung ...",
-        "en": "... but only on your responsibility ..."
+        "en": "... but only on your responsibility ...",
+        "no": "... men kun på ditt ansvar ..."
     },
     {
         "Kapitel": "1_TN_01_10",
         "de": "... und ich drehe um, wenn es zu riskant wird.",
-        "en": "... and I turn back when it gets too risky."
+        "en": "... and I turn back when it gets too risky.",
+        "no": "... og jeg vil snu når det blir for risikabelt"
     },
     {
         "Kapitel": "1_TN_01_13",
         "de": "Sicher, dass Sie es versuchen wollen, Junge? ",
-        "en": "Are you sure you want to try it, boy?"
+        "en": "Are you sure you want to try it, boy?",
+        "no": "Er du sikker på at vil prøve, gutt?"
     },
     {
         "Kapitel": "1_TN_01_14_1",
         "de": "Ahoi, Bill",
-        "en": "Ahoy, Bill"
+        "en": "Ahoy, Bill",
+        "no": "Ahoy, Bill"
     },
     {
         "Kapitel": "1_TN_01_14_2",
         "de": "Holt den Captain!",
-        "en": "Get the captain!"
+        "en": "Get the captain!",
+        "no": "Hent kapteinen"
     },
     {
         "Kapitel": "1_TN_01_14_3",
         "de": "Keine Chance, Junge.",
-        "en": "Not a chance, boy."
+        "en": "Not a chance, boy.",
+        "no": "Ikke en sjanse, gutt"
     }
 ]
