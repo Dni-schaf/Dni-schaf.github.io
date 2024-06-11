fb55d0a666240a099b6b2cc13f383d64e84026f4
Dni-schaf <d.scharffenberg@web.de>
2024-06-11 20:21:40 +0200

    new try for map and comic

---
+++ resources/js/comicAndMap_images.js
@@ -0,0 +1,40 @@
+const imageSets = [
+    [
+        { "Kapitel": "01_TN_01", "speed": 0.05 },
+        { "Kapitel": "01_TN_02", "speed": 0 },
+        { "Kapitel": "01_TN_03", "speed": 0 },
+        { "Kapitel": "01_TN_04", "speed": 0 },
+        { "Kapitel": "01_TN_05", "speed": 0 },
+        { "Kapitel": "01_TN_06", "speed": 0 },
+        { "Kapitel": "01_TN_07", "speed": 0 },
+        { "Kapitel": "01_TN_08", "speed": 0 },
+        { "Kapitel": "01_TN_09", "speed": 0.05 },
+        { "Kapitel": "01_TN_10", "speed": 0 },
+        { "Kapitel": "01_TN_11", "speed": 0.05 },
+        { "Kapitel": "01_TN_12", "speed": 0 }
+    ],
+    [
+        { "Kapitel": "02_TN_01", "speed": 0 },
+        { "Kapitel": "02_TN_02", "speed": 0 },
+        { "Kapitel": "02_TN_03", "speed": 0 },
+        { "Kapitel": "02_TN_04", "speed": 0 }
+    ],
+    [
+        { "Kapitel": "03_TN_01", "speed": 0 },
+        { "Kapitel": "03_TN_02", "speed": 0 },
+        { "Kapitel": "03_TN_03", "speed": 0 },
+        { "Kapitel": "03_TN_04", "speed": 0 }
+    ],
+    [
+        { "Kapitel": "04_TN_01", "speed": 0 },
+        { "Kapitel": "04_TN_02", "speed": 0 },
+        { "Kapitel": "04_TN_03", "speed": 0 },
+        { "Kapitel": "04_TN_04", "speed": 0 }
+    ],
+    [
+        { "Kapitel": "05_TN_01", "speed": 0 },
+        { "Kapitel": "05_TN_02", "speed": 0 },
+        { "Kapitel": "05_TN_03", "speed": 0 },
+        { "Kapitel": "05_TN_04", "speed": 0 }
+    ] 
+];
