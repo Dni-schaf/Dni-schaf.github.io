802ebf5e1243334bede66d3d38779a06ada10a82
Dni-schaf <d.scharffenberg@web.de>
2026-06-07 20:44:44 +0200

    new index translation

---
+++ resources/js/index_text.js
@@ -0,0 +1,48 @@
+const index_txt = {
+  "title": {
+      "text_de": "Ein Palast aus Eis </br> am Ende der Welt",
+      "text_en": "A palace of ice </br> at the end of the world",
+      "text_no": "Et ispalass </br> ved verdens ende",
+    },
+  "present": {
+      "text_de": "Ein Projekt von:",
+      "text_en": "A project by:",
+      "text_no": "NO",
+    },
+    "headline": {
+      "text_de": "Wir schreiben das Jahr 1910. Zwei Männer. Zwei Expeditionen. Ein Ziel.",
+      "text_en": "EN",
+      "text_no": "NO",
+    },
+    "p1": {
+      "text_de": "Der Brite Robert Falcon Scott und der Norweger Roald Amundsen brechen zu einer abenteuerlichen Reise auf, bei der sie sich durch das unwirtlichste Terrain der Erde kämpfen werden – getrieben von Ehrgeiz, Pflichtgefühl und dem unbändigen Willen, als Erste den Südpol zu erreichen. Was in den Geschichtsbüchern als sportlicher Wettlauf gilt, war in Wirklichkeit ein Kampf ums Überleben – gegen Kälte, Erschöpfung, Zweifel und die schiere Endlosigkeit des weißen Kontinents.",
+      "text_en": "EN",
+      "text_no": "NO",
+    },
+    "p2": {
+      "text_de": "Basierend auf den Originaltagebüchern der Expeditionen erweckt Ein Palast aus Eis am Ende der Welt eine der dramatischsten Geschichten der Entdeckungsgeschichte neu zum Leben. In über 500 Bildern und sechs Kapiteln erzählen wir, was XXXXXXXX und folgen beiden Männern auf Schritt und Tritt – nah, persönlich und packend. Was die Tagebücher überliefern – und was zwischen den Zeilen liegt.",
+      "text_en": "EN",
+      "text_no": "NO",
+    },
+    "p3": {
+      "text_de": "Eine digitale Graphic Novel für alle, die spannende Geschichten lieben – und nebenbei etwas über Mut, Strategie und die Grenzen des Möglichen lernen wollen.",
+      "text_en": "EN",
+      "text_no": "NO",
+    },
+    "call_to_action": {
+      "text_de": "Der erste Teil ist jetzt verfügbar. Machen Sie sich einen Tee, legen Sie die Füße hoch und XXXXXXX",
+      "text_en": "EN",
+      "text_no": "NO",
+    },
+    "button_txt": {
+      "text_de": "Leinen Los!",
+      "text_en": "EN",
+      "text_no": "NO",
+    },
+    "impressum": {
+      "text_de": "Verantwortlich für den Inhalt der Seite ist Daniela Scharffenberg</br>Kontakt E-Mail: kontakt@dna-graphic-novals</br>Ein Projekt von DNA Graphic Novels",
+      "text_en": "EN",
+      "text_no": "NO",
+    },
+  
+}
